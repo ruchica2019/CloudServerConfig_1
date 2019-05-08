c4d4e853ffe1b55076b824665f4f15d350847aac
ruchica2019 <46400267+ruchica2019@users.noreply.github.com>
2019-05-08 12:19:59 +0530

    Baseline committed

---
+++ src/main/java/com/cloudserverconfig/CloudServerConfig/CloudServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cloudserverconfig.CloudServerConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerConfigApplication.class, args);
+	}
+
+}
